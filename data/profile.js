108be96d27f6544da990118835097893999c53f7
“akshaya1004” <akshayadikshit1004@gmail.com>
2022-02-19 15:53:30 +0530

    initial commit

---
+++ data/profile.js
@@ -0,0 +1,182 @@
+const profileData = {
+  title: 'Profile',
+  name: 'Akshaya Narayan Dikshit',
+  sub_title: 'Java and Android Developer',
+  about: {
+    intro: `Android Developer Geek`,
+    contact: {
+      email: 'akshayadikshit1004.com',
+      phone: '+91-9559421992',
+      address: 'Bangalore, India'
+    }
+  },
+  links: [
+    { title: 'Linkedin', src: 'https://www.linkedin.com/in/akshayadikshit/' },
+    { title: 'Github', src: 'https://github.com/akshaya1004' }
+  ],
+  education: [
+    {
+      alma: 'Mtech (CS)',
+      duration: 'in-progress',
+      std: 'BITS-WILP',
+
+    },
+
+    {
+      alma: 'Btech (CS)',
+      duration: '2011- 2015',
+      std: 'PSIT College',
+
+    },
+  ],
+  skills: [
+    {
+      category: 'Android',
+      topics: ['MVVM', 'MVP', 'DAGGER-HILT', 'Jetpack', 'Coroutines', 'Rxjava', 'Livedata', 'Junit', 'Mockito','Retrofit', 'Picasso', 'Glide', 'etc']
+    },
+    {
+      category: 'Programming Language',
+      topics: [
+        'Java',
+        'Kotlin',
+        'XML',
+        'Solidity',
+        'node js '
+      ]
+    },
+    {
+      category: 'Android Expertise',
+      topics: ['Security', 'Size Optimization', 'Performance', 'etc']
+    },
+    {
+      category: 'Exploring Currently',
+      topics: ['Blockchain', 'Ethereum DAPPS', 'Jetpack Compose', 'etc']
+    }
+  ],
+  projects: [
+    {
+      title: 'Dominos India App',
+      link: 'https://play.google.com/store/apps/details?id=com.Dominos',
+      desc: `Dominos pizza delivery app comes with an easy-to-use interface, a quick selection menu, and a
+smartly designed app-interface that lets you order food online of your choice`
+    },
+    {
+      title: 'Dominos Bangladesh App',
+      link: 'https://play.google.com/store/apps/details?id=com.dominos.bd',
+      desc: `Dominos pizza delivery app comes with an easy-to-use interface, a quick selection menu, and a
+smartly designed app-interface that lets you order food online of your choice`
+},{
+  title: 'Novopay Retailer App',
+  link: 'https://play.google.com/store/apps/details?id=in.novopay.merchant&hl=en_IN',
+  desc:`Money Transfer feature,, Aadhar ATM, Cash Services, Recharges, DTH Recharges, Healthcare, etc`
+},{
+title: ' Novopay LeadR App',
+link: 'https://play.google.com/store/apps/details?id=in.novopay.leadrapp',
+desc: `Features include Task Management, On-boarding of new Retailers/ Distributors/ SuperDistributors.
+`
+},{
+title: 'Novopay Grahak App',
+link: 'https://play.google.com/store/apps/details?id=in.novopay.consumerapp',
+desc: `It helps in tracking transactions such as cash deposits, withdrawals, bill payments, recharges, etc.`
+}
+,{
+title: 'SureLock Kiosk Lockdown ',
+link: 'https://play.google.com/store/apps/details?id=com.gears42.surelock',
+desc: `SureLock uses Android Device Administrator permissions for advanced lock-down features.
+SureLock is the #1 kiosk lockdown tool for Android that quickly transforms any Android tablet or smartphone into a dedicated Android kiosk. It replaces the default Home Screen or Launcher and restricts access to only admin-approved applications. SureLock has built-in device management capabilities.`
+},{
+title: 'SureMDM Mobile Device Management - 42Gears MDM ',
+link: 'https://play.google.com/store/apps/details?id=com.nix',
+desc:`SureMDM is a leading Mobile Device Management solution used by over ten thousand companies worldwide to manage their fleet of Android devices, plus devices running other platforms like Windows, iOS, and Linux. Remotely deploy mobile apps and content, secure, track, and troubleshoot devices - all from a central web console.`
+},{
+title: 'SureFox Kiosk Browser Lockdown ',
+link: 'https://play.google.com/store/apps/details?id=com.gears42.surefox',
+desc:`SureFox is a lockdown browser you can use for controlling what web sites your users can browse on Android tablets and smartphones.
+ This can be very useful if you want to deploy Android tablets in public areas such as restaurants, cabs, trade shows, conferences, libraries, schools, hospital waiting lounges, field force etc and you want to ensure that the users can only access allowed website and nothing else on the tablets.`
+},
+{
+title: 'SureVideo Kiosk Video Looper ',
+link: 'https://play.google.com/store/apps/details?id=com.gears42.surevideo',
+desc: `Play one or more videos continuously in a loop on Android tablets.
+Viewers can watch the videos in fullscreen mode but cannot do anything else on the device. Tablet will work only as a video playing display device. Can be used in trade shows, exhibitions and other community events.`
+},{
+title: 'Explara Event Manager ',
+link: 'https://play.google.com/store/apps/details?id=explara.eventmanager',
+desc:`The Explara Event Manager App is specially designed for the modern event organizer. You can now create beautiful and fully functional events on-the-go while keeping track of your other events' attendees.
+You can scan issued tickets, sell tickets on-the-spot and handle multiple guest lists from a single app! The Explara Event Manager is directly linked to your Explara account and all you need to do is Log In and you're good to go.`
+},{
+title: 'Explara Community app ',
+link: 'https://play.google.com/store/apps/details?id=com.explara.community',
+desc:`The Explara Community app to connect socially with all social platform linking and event details.`
+},{
+title: ' Explara app',
+link: 'https://play.google.com/store/apps/details?id=com.explara.android',
+desc:`The Explara app is event ticketing platform where all the events listed with categories, trending.`
+}
+
+
+
+  ],
+  experiences: [
+    {
+      organization: 'Jubilant Food Works Ltd ,   Senior Software Developer   (April 2021 till present)',
+      title: `SDE-3`,
+      date: 'April 2021 - till present',
+      details: [
+        `Dominos pizza delivery app comes with an easy-to-use interface, a quick selection menu, and a smartly designed app-interface that lets you order food online of your choice`
+      ]
+    },
+    {
+      organization: 'Novopay Solutions Pvt Ltd , Bangalore - Mobile Application Developer  Fintech (March 2018 till April 2021 )',
+      title: `SDE-2`,
+      date: 'March 2018 - April 2021',
+      details: [
+        `Money Transfer feature,Aadhar ATM, Cash Services, Recharges, DTH Recharges, Healthcare, etc`,
+        ` Features include Task Management, On-boarding of new Retailers/ Distributors/ SuperDistributors. It helps in tracking transactions such as cash deposits, withdrawals, bill payments, recharges, etc`
+      ]
+    },
+    {
+      organization: '42 Gears Mobility System, Bangalore  (Oct 2016 till Feb 2018)',
+      title: `SDE-1`,
+      date: 'Oct 2016 - Feb 2018',
+      details: [
+        `42Gears MDM - Leader in enterprise mobility management, unified endpoint management, & mobile device management which helps to secure and manage endpoints`,
+      ]
+    },
+      {
+        organization: 'Explara, Bangalore- Android Developer (Aug 2015 till Aug 2016)',
+        title: `Software Engineer`,
+        date: 'Aug 2015 - Aug 2016',
+        details: [
+          `Explara is an all-in-one platform for creators and small businesses to monetize audiences in multiple ways and grow revenue and Event Ticketing platform`,
+        ]
+      }
+  ],
+  certifications: [
+    {
+      desc: `Awarded by Novopay for Employee of the month.`,
+    },
+    {
+      desc: `Awarded by 42Gears for Employee of the month`,
+    },
+    {
+      desc: `Java Programming Language using Java SE6 by JLC.`,
+    },
+    {
+      desc: `Workshop on Ethical Hacking in IIT Kanpur.`,
+    },
+    {
+      desc: `Workshop on Android Development in IIT Kanpur`,
+    },
+    {
+      desc: `Developed some Landing pages using Wordpress.`,
+    },
+    {
+      desc: `Participated  as a Speaker in TechTalks in Android meetup Novopay.`,
+    },
+    {
+      desc: `Android Engineer Volunteer in FightPandemics`,
+    }
+  ],
+  events: []
+};
